390f217021a0a60457196862d4faac9929dbfbf7
GhazaleBorna <147149552+GhazaleBorna@users.noreply.github.com>
2024-09-27 14:03:40 +0200

    Update index.php

--- Website/Style/index.php
+++ Website/Style/index.php
@@ -1,1 +1,28 @@
+<?php 
+
+session_start();
+
+if( !isset($_SESSION['token']) ) {
+	$_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(32));
+}
+
+if(isset($_SESSION['meldung'])) {
+	$meldung = $_SESSION['meldung'];
+	unset($_SESSION['meldung']);
+}
+
+require_once 'inc/datenbank.inc.php';
+require_once 'inc/funktionen.inc.php';  
+
+$page = $_GET['page'] ?? '';
+
+require_once 'inc/header.inc.php';
+
+switch($page) {
+	case 'registrierung': include 'inc/register_form.inc.php'; break;
+	case 'profil': include 'inc/profil.inc.php'; break;
+	default: include 'inc/home.inc.php'; break;
+}
+
+require_once 'inc/footer.inc.php';
 
