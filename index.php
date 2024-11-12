2d94e61e2fdd3507b9c57deb147d38b498585291
GhazaleBorna <147149552+GhazaleBorna@users.noreply.github.com>
2024-11-12 15:56:20 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php 
 
 session_start();
-
+/*
 if( !isset($_SESSION['token']) ) {
 	$_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(32));
 }
-
+*/
 if(isset($_SESSION['meldung'])) {
 	$meldung = $_SESSION['meldung'];
 	unset($_SESSION['meldung']);
@@ -20,7 +20,6 @@ require_once 'inc/header.inc.php';
 
 switch($page) {
 	case 'registrierung': include 'inc/register_form.inc.php'; break;
-	case 'profil': include 'inc/profil.inc.php'; break;
 	default: include 'inc/home.inc.php'; break;
 }
 
