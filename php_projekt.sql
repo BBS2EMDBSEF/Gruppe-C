8a568a86d1b33a762578d388c5d39d9761fc7fa0
GhazaleBorna <147149552+GhazaleBorna@users.noreply.github.com>
2024-11-15 12:14:38 +0100

    Update php_projekt.sql

--- php_projekt.sql
+++ php_projekt.sql
@@ -1,8 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
---
 -- Datenbank: `php_projekt`
 --
 CREATE DATABASE IF NOT EXISTS `php_projekt` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
