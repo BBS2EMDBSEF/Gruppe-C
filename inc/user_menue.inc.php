36f089a409379d5a7ce885b2a6d81619f1c0b390
GhazaleBorna <147149552+GhazaleBorna@users.noreply.github.com>
2024-11-19 12:35:04 +0100

    Update user_menue.inc.php

--- inc/user_menue.inc.php
+++ inc/user_menue.inc.php
@@ -13,6 +13,20 @@
 		<input type="file" name="file" id="file">
 		<input type="submit" value="Hochladen" name="submit">
 	</form>
+	<h3>Hochgeladene Dateien:</h3>
+  	<ul>
+    		<?php
+    		require 'inc/datenbank.inc.php'; // Datei mit der Datenbankverbindung
+    		$stmt = $db->prepare("SELECT * FROM files WHERE user_id = ?");
+    		$stmt->execute([$_SESSION['id']]);
+    		$files = $stmt->fetchAll();
+
+    		foreach ($files as $file) {
+      			echo '<li><a href="inc/download.php?file_id/' . htmlspecialchars($file['id']) . '">' . 
+        		htmlspecialchars($file['filename']) . '</a></li>';
+    		}
+    		?>
+  	</ul>
 
 </div>
 
