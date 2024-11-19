ddf6cb89edbafb08ff2be3540588aaceee403a2b
GhazaleBorna <147149552+GhazaleBorna@users.noreply.github.com>
2024-11-19 12:28:41 +0100

    Update delete.php

--- inc/delete.php
+++ inc/delete.php
@@ -1,18 +1,3 @@
 <?php 
-/*
-session_start();
-require_once 'datenbank.inc.php';
-require_once 'funktionen.inc.php';
 
-$id = $_SESSION['id'];
-
-if( !empty($_POST) && $_POST['csrf_token'] === $_SESSION['token']) {
-  $sql = 'DELETE FROM users WHERE id = ? ';
-  $statement = $db->prepare($sql);
-  $statement->execute([$id]);
-  loggeAus();
-  $_SESSION['meldung'] = 'account wurde gelöscht';
-}
-
-redirect('../index.php');
 */
