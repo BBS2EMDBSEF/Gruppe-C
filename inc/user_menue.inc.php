c589ba16d6975367dd86b0b1ed84b9b17e6c1a77
GhazaleBorna <147149552+GhazaleBorna@users.noreply.github.com>
2024-11-19 12:33:33 +0100

    Update user_menue.inc.php

--- inc/user_menue.inc.php
+++ inc/user_menue.inc.php
@@ -1,10 +1,18 @@
 <div class="userMenue">
-<ul>
-	<li>
+	<ul>
+		<li>
 		Ihre Konto
-	</li>
-	<li><a href="inc/logout.php">Logout</a></li>
-</ul>
+		</li>
+		<li><a href="inc/logout.php">Logout</a></li>
+	</ul>
 
-<p>Eingeloggt als: <em><?= $_SESSION['eingeloggt_user']?></em> </p>
+	<p>Eingeloggt als: <em><?= $_SESSION['eingeloggt_user']?></em> </p>
+
+	<form action="inc/upload.php" method="post" enctype="multipart/form-data">
+		<label for="file">Datei hochladen:</label>
+		<input type="file" name="file" id="file">
+		<input type="submit" value="Hochladen" name="submit">
+	</form>
+
 </div>
+
