10d210b08c227f4da285ef79e33e68b59250c2ae
GhazaleBorna <147149552+GhazaleBorna@users.noreply.github.com>
2024-11-29 15:01:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,16 +8,10 @@ if(isset($_SESSION['meldung'])) {
 }
 
 require_once 'inc/datenbank.inc.php';
-require_once 'inc/funktionen.inc.php';  
 
 $page = $_GET['page'] ?? '';
-
-require_once 'inc/header.inc.php';
 
 switch($page) {
 	case 'registrierung': include 'inc/register_form.inc.php'; break;
 	default: include 'inc/home.inc.php'; break;
 }
-
-require_once 'inc/footer.inc.php';
-
