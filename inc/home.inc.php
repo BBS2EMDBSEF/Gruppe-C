ac2d322c7d89c776dc419028f91bc4541f74a056
GhazaleBorna <147149552+GhazaleBorna@users.noreply.github.com>
2024-11-29 15:02:35 +0100

    Update home.inc.php

--- inc/home.inc.php
+++ inc/home.inc.php
@@ -1,11 +1,88 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="utf-8" />
+	<title>PHP-Projekt</title>
+	<style>
+	<?php
+		include 'css/master.css';
+		include 'css/fonts.css';
+		include 'css/media.css';
+	?>
+	</style>
+</head>
+<body>
+<div id="container">
+<header>
+<a class="logo" href="index.php?page=home"><img src="img/logo.png" alt="" width="150" height="100"></a>
+</header>
+
+<!--++++++MENUE++++-->
+<div class="container">
+
+<nav title="Menü">
+  <a href="#open" class="open" id="open">open</a>
+  <a href="#" class="close">close</a>
+
+  <ul class="resp-menu">
+
+	  <li class="<?= empty($page) ? 'active' : ''; ?>">
+		  <a href="index.php" title="Home">HOME</a>
+	  </li>
+
+		<li id="liRegister" class="<?php echo ($page === 'registrierung') ? 'active' : ''; ?>">
+		      <a href="index.php?page=registrierung">REGISTRIERUNG</a>
+	  </li>
+
+  </ul>
+</nav>
+
+<?php if(isset($meldung)): ?>
+	<p class="msg"> <?= $meldung ?> </p>
+<?php endif;
+
+//wenn ein user angemeldet ist dann das zeigen
+if(isset($_SESSION['eingeloggt'])):
+	require_once 'inc/user_menue.inc.php';
+  $_SESSION['meldung'] = 'Sie sind eingeloggt';
+endif;
+?>
+
+<main>
+
 <div class="home">
-   
+  <?php 
+  if(!isset($_SESSION['eingeloggt'])) {?>
+  <div class="loginForm">
+    <form action="inc/login_check.php" method="post">
+      <input type="text" name="mail" placeholder="E-Mail eingeben" />
+      <input type="password" name="pwd" placeholder="Passwort" />
+      <input class="subLoggin" type="submit" value="Einloggen" />
+    </form>
+
+  <p> <a href="index.php?page=registrierung">Registrierung</a> </p>
+</div>
+<?php
+  }
+  ?>
+
   <div class="text">			
      <h1>Herzlich Willkommen</h1>
-     
   </div>
+  
 </div>
 
+</main>
+
+</div> <!--ende container-->
+<!--++++++++++--> 
+<footer>
+<p>BBS2</p>
+</footer>
+
+</div><!--ende container-->
+</body>
+</html>
 
 
 
