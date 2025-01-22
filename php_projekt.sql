823f2b21c275eb792027f17cb7406ee2cbe18d1b
GhazaleBorna <147149552+GhazaleBorna@users.noreply.github.com>
2025-01-22 09:54:06 +0100

    Update php_projekt.sql
    Signed-off-by: GhazaleBorna <147149552+GhazaleBorna@users.noreply.github.com>

--- php_projekt.sql
+++ php_projekt.sql
@@ -10,7 +10,7 @@ CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
   `nachname` varchar(255) NOT NULL,
   `vorname` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `passwort` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -18,16 +18,16 @@ CREATE TABLE `users` (
 -- Daten für Tabelle `users`
 --
 
-INSERT INTO `users` (`id`, `nachname`, `vorname`, `email`, `passwort`) VALUES
-(75, 'Borna', 'Ghazaleh', 'borna@borna.de', '$2y$10$mJ75vpei0M2ElJDZMwEOhu2LUu3Ng8MEHQPBqCXA5CRegaCnkeF0K'),
-(76, 'admin', 'admin', 'admin@admin.de', '$2y$10$0bKqPZ80Uokt8Y8bTjKroup6rQGYO6PBMi8RbqaOa7B6SEClO7T7.');
+INSERT INTO `users` (`id`, `nachname`, `vorname`, `username`, `passwort`) VALUES
+(75, 'Borna', 'Ghazaleh', 'borna', '$2y$10$mJ75vpei0M2ElJDZMwEOhu2LUu3Ng8MEHQPBqCXA5CRegaCnkeF0K'),
+(76, 'admin', 'admin', 'admin', '$2y$10$0bKqPZ80Uokt8Y8bTjKroup6rQGYO6PBMi8RbqaOa7B6SEClO7T7.');
 
 
 -- Indizes für die Tabelle `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT für Tabelle `users`
