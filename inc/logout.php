0008a0a796e9a38de68c0cd5f65f57ef22ffdd0d
GhazaleBorna <147149552+GhazaleBorna@users.noreply.github.com>
2025-01-22 09:42:30 +0100

    Update logout.php
    Signed-off-by: GhazaleBorna <147149552+GhazaleBorna@users.noreply.github.com>

--- inc/logout.php
+++ inc/logout.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 session_start();
 
@@ -7,5 +7,5 @@ unset($_SESSION['eingeloggt_user']);
 unset($_SESSION['id']);
 $_SESSION['meldung'] = 'Sie sind abgemeldet.';
 
-header('Location:' . '../index.php');
-exit;
+header('Location:'.'../index.php');
+  exit;
