a7012654d9b72c177c30406bb6b1aeecbd730ea0
Levin <lol.wieching@gmail.com>
2025-01-27 11:29:31 +0100

    aufräumen

--- Webseite_Funktionalität_noch_nicht_fertig/download.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dir = '/';
-$files = array_diff(scandir($dir), array('.', '..'));
-
-foreach ($files as $file) {
-    echo "<a href='uploads/$file' download>$file herunterladen</a><br>";
-}
-?>
